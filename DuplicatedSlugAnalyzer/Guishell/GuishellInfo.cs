209daf7957c26641c8306e124d9ce41be02a59c7
Enrico Massone <enrico.massone@deltatre.it>
2018-07-23 12:15:08 +0200

    Added some argument validation GuishellInfo class

--- DuplicatedSlugAnalyzer/Guishell/GuishellInfo.cs
+++ DuplicatedSlugAnalyzer/Guishell/GuishellInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace DuplicatedSlugAnalyzer.Guishell
+﻿using System;
+
+namespace DuplicatedSlugAnalyzer.Guishell
 {
 	public class GuishellInfo
 	{
@@ -8,6 +10,15 @@
 
 		public GuishellInfo(string guishellBaseUrl, string applicationName, string guishellSecret)
 		{
+			if(string.IsNullOrWhiteSpace(guishellBaseUrl))
+				throw new ArgumentException("Guishell base url cannot be null or white space.", nameof(guishellBaseUrl));
+
+			if (string.IsNullOrWhiteSpace(applicationName))
+				throw new ArgumentException("Guishell application name cannot be null or white space.", nameof(applicationName));
+
+			if (string.IsNullOrWhiteSpace(guishellSecret))
+				throw new ArgumentException("Guishell secret cannot be null or white space.", nameof(guishellSecret));
+
 			this.GuishellBaseUrl = guishellBaseUrl;
 			this.ApplicationName = applicationName;
 			this.GuishellSecret = guishellSecret;
