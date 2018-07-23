9a191bce5e49f5574660f475772358f7f7ffc4bb
Enrico Massone <enrico.massone@deltatre.it>
2018-07-23 12:06:39 +0200

    Added some debug console writelines...

--- DuplicatedSlugAnalyzer/Program.cs
+++ DuplicatedSlugAnalyzer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using DuplicatedSlugAnalyzer.Guishell;
 using DuplicatedSlugAnalyzer.Mongodb;
 using Microsoft.Extensions.Configuration;
@@ -25,36 +26,34 @@ namespace DuplicatedSlugAnalyzer
 
 		private static async Task RunAsync(string[] args)
 		{
+			WriteLine("Welcome to Webplu Duplicate Slugs Analyzer. Press enter to start.");
+
 			WriteLine("Reading configuration to get guishell info...");
-
 			var config = GetConfiguration(args);
-
 			var guishellBaseUrl = config[GuishellBaseUrlConfigKey];
 			var applicationName = config[ApplicationNameConfigKey];
 			var guishellSecret = config[GuishellSecretConfigKey];
 
 			WriteLine("Calling guishell to get Forge configuration...");
-
 			var guishellInfo = new GuishellInfo(guishellBaseUrl, applicationName, guishellSecret);
 			var guishellAppConfiguration = await GetGuishellAppConfigurationAsync(guishellInfo)
 				.ConfigureAwait(false);
 
 			WriteLine("Querying backoffice database to get all duplicated slugs for published entities...");
-
 			var mongodbConnString = guishellAppConfiguration.BackEndStoreConfiguration.ConnectionString;
 			var duplicateSlugFinder = DuplicateSlugsFinderFactory.Create(mongodbConnString);
-			var duplicateSlugsInfos = await duplicateSlugFinder
+			var duplicateSlugsInfos = (await duplicateSlugFinder
 				.GetDuplicateSlugsInfoAsync()
-				.ConfigureAwait(false);
+				.ConfigureAwait(false)).ToArray();
+			WriteLine($"Found ${duplicateSlugsInfos.Length} duplicated slug reservation keys.");
 
-			WriteLine("Preparing JSON report...");
-
+			WriteLine($"Preparing JSON report {ReportFileName}. Report will be saved under the folder {ReportDirectoryName} which is located at the same level of the executable file.");
 			await CreateJsonReportAsync(
 				duplicateSlugsInfos, 
 				ReportFileName, 
 				ReportDirectoryName).ConfigureAwait(false);
 
-			WriteLine("All done");
+			WriteLine("Execution successfully completed. Press enter to close.");
 		}
 
 		private static IConfiguration GetConfiguration(string[] args)
