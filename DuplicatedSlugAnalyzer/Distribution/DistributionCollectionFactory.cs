44f9f461479e3d28d84fdcaefc6d3993e85bcc53
Enrico Massone <enrico.massone@deltatre.it>
2018-07-23 13:02:17 +0200

    work in progress

--- DuplicatedSlugAnalyzer/Distribution/DistributionCollectionFactory.cs
+++ DuplicatedSlugAnalyzer/Distribution/DistributionCollectionFactory.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using MongoDB.Bson;
 using MongoDB.Driver;
+using static DuplicatedSlugAnalyzer.Mongodb.MongoDbHelpers;
 
 namespace DuplicatedSlugAnalyzer.Distribution
 {
 	public class DistributionCollectionFactory
 	{
 		private readonly IMongoDatabase _database;
+		private readonly IReadOnlyDictionary<string, string> _entityCodeToDistributionCode;
 
-		private DistributionCollectionFactory(IMongoDatabase database)
+		public static DistributionCollectionFactory Create(
+			string connString, 
+			IReadOnlyDictionary<string, string> entityCodeToDistributionCode)
+		{
+			if (entityCodeToDistributionCode == null)
+				throw new ArgumentNullException(nameof(entityCodeToDistributionCode));
+
+			var database = GetDatabaseFromConnString(connString);
+
+			return new DistributionCollectionFactory(database, entityCodeToDistributionCode);
+		}
+
+		private DistributionCollectionFactory(
+			IMongoDatabase database, 
+			IReadOnlyDictionary<string, string> entityCodeToDistributionCode)
 		{
 			_database = database;
+			_entityCodeToDistributionCode = entityCodeToDistributionCode;
+		}
+
+		public IMongoCollection<BsonDocument> GetCollection()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
