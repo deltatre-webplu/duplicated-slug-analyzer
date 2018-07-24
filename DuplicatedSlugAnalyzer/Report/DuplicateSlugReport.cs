3dbb801f7c9e4da7e911fcda5c94a3d7f453f81e
Enrico Massone <enrico.massone@deltatre.it>
2018-07-24 10:33:07 +0200

    Introduced report specific classes (forge entity and duplicate slug report)

---
+++ DuplicatedSlugAnalyzer/Report/DuplicateSlugReport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DuplicatedSlugAnalyzer.Forge;
+
+namespace DuplicatedSlugAnalyzer.Report
+{
+	public class DuplicateSlugReport
+	{
+		public SlugReservationKey Key { get; }
+		public int NumberOfEntities { get; }
+		public IEnumerable<ForgeEntity> Entities { get; }
+
+		public DuplicateSlugReport(
+			SlugReservationKey key,
+			int numberOfEntities,
+			IEnumerable<ForgeEntity> entities)
+		{
+			Key = key;
+			NumberOfEntities = numberOfEntities;
+			Entities = entities;
+		}
+	}
+}
---
+++ DuplicatedSlugAnalyzer/Report/ForgeEntity.cs
@@ -0,0 +1,16 @@
+﻿using DuplicatedSlugAnalyzer.Forge;
+
+namespace DuplicatedSlugAnalyzer.Report
+{
+	public class ForgeEntity
+	{
+		public ForgeEntity(EntityIdentifier identifier, bool isPublished)
+		{
+			Identifier = identifier;
+			IsPublished = isPublished;
+		}
+
+		public EntityIdentifier Identifier { get; }
+		public bool IsPublished { get; }
+	}
+}
