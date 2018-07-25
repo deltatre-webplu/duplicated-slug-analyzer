4c2c92ca1e9fda475601ba7fbbd36a47bcc668ad
Enrico Massone <enrico.massone@deltatre.it>
2018-07-25 13:39:53 +0200

    Introduced EntityPublishedDuplicateSlugsFinder (not used right now)

---
+++ DuplicatedSlugAnalyzer/New_Stuff/DuplicateSlugInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using DuplicatedSlugAnalyzer.Forge;
+
+namespace DuplicatedSlugAnalyzer.New_Stuff
+{
+	public class DuplicateSlugInfo
+	{
+		public SlugReservationKey Key { get; }
+		public HashSet<EntityIdentifier> EntityIdentifiers { get; }
+
+		public DuplicateSlugInfo(
+			SlugReservationKey key, 
+			HashSet<EntityIdentifier> entityIdentifiers)
+		{
+			Key = key;
+			EntityIdentifiers = entityIdentifiers;
+		}
+	}
+}
